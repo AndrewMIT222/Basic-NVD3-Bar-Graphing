0f70b0b225bf39dc3f71c1609c5b983953c08c76
Andrew Ressler 2 <andrew.ressler.2@gmail.com>
2016-08-01 10:24:01 -0400

    Set up to read from a json file, & added a route to display the json file itself.

--- running_bar_graph.py
+++ running_bar_graph.py
@@ -3,18 +3,23 @@ from flask import *
 
 app = Flask(__name__)
 
-#Flask(__name__, template_folder="templates/bar_graph_full.html")
+# Flask(__name__, template_folder="templates/bar_graph_full.html")
 
 
 @app.route('/')
 def run_bar_graph():
 
-    return "test" + render_template('bar_graph_full.html')
+    return render_template('bar_graph_full.html')
 
 
 @app.route('/hello')
 def hello_world():
     return 'Hello world!'
+
+@app.route('/json')
+def return_json():
+    return send_from_directory('static', 'nv.d3_example_graph1.json')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
