1a3a30d6af85fd845ba48b9c04176f08add00999
Andrew Ressler 2 <andrew.ressler.2@gmail.com>
2016-08-11 10:44:21 -0400

    Got program to one page & chart label names sensible & centered.

--- running_bar_graph.py
+++ running_bar_graph.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 # runOnVoodoo = False
 
 
-@app.route('/')
+@app.route('/trial')
 def run_bar_graph():
         return render_template('form.html')
 
@@ -26,61 +26,72 @@ def hello_world():
     return 'Hello world!'
 
 
-@app.route('/form_submit')
+@app.route('/')
 def return_json():
-
-    runOnVoodoo = request.args.get('option')
-    if runOnVoodoo == 'voodoo':
-        dbconnector = voodoo
-    else:
-        dbconnector = monetdb
-
-    connection = dbconnector.sql.connect(username="monetdb", password="monetdb", hostname="localhost", database="simple")
-    cursor = connection.cursor()
-    c = request.args.get('query')
-    cursor.execute(c)
-    b = cursor.fetchall()
-    '''
-    a = {'yVals': [[37734107, 991417, 74476040, 37719753],
-         [56586554400.73, 1487504710.38, 111701729697.74, 56568041380.9],
-         [53758257134.87, 1413082168.05, 106118230307.61, 53741292684.6],
-         [55909065222.83, 1469649223.19, 110367043872.5, 55889619119.83],
-         [25.52, 25.52, 25.5, 25.51],
-         [38273.13, 38284.47, 38249.12, 38250.86],
-         [0.05, 0.05, 0.05, 0.05],
-         [1478493, 38854, 2920374, 1478870]], 'xVals': ["AF", "NF", "NO", "RF"]}
-    '''
-
-    cursor2 = connection.cursor()
-    d = "trace " + request.args.get('query')
-    cursor2.execute(d)
-    e = cursor2.fetchall()
-
     def decimal_default(obj):
         if isinstance(obj, decimal.Decimal):
             return float(obj)
         raise TypeError
 
-    xVals = []
-    yVals = []
+    query = request.args.get('query')
+    if query == None:
+        a = {"yVals": [], "xVals": [], "runTime": 0}
 
-    for x in b:
-        xVals.append(x[0]+x[1])
-    count = 2
-    for x in range((len(b[0])) - 2):
-        yInnerList = []
-        for y in b:
-            yInnerList.append(y[count])
-        count += 1
-        yVals.append(yInnerList)
+        data = json.dumps(a, default=decimal_default)
 
-    timeTook = e[-1][0]
+        a = json.loads(data)
+    else:
+        runningOn = request.args.get('option')
+        if runningOn == 'voodoo':
+            dbconnector = voodoo
+        else:
+            dbconnector = monetdb
 
-    a = {"yVals": yVals, "xVals": xVals, "runTime": timeTook}
+        connection = dbconnector.sql.connect(username="monetdb", password="monetdb", hostname="localhost", database="simple")
+        cursor = connection.cursor()
+        cursor.execute(query)
+        b = cursor.fetchall()
+        '''
+        a = {'yVals': [[37734107, 991417, 74476040, 37719753],
+             [56586554400.73, 1487504710.38, 111701729697.74, 56568041380.9],
+             [53758257134.87, 1413082168.05, 106118230307.61, 53741292684.6],
+             [55909065222.83, 1469649223.19, 110367043872.5, 55889619119.83],
+             [25.52, 25.52, 25.5, 25.51],
+             [38273.13, 38284.47, 38249.12, 38250.86],
+             [0.05, 0.05, 0.05, 0.05],
+             [1478493, 38854, 2920374, 1478870]], 'xVals': ["AF", "NF", "NO", "RF"]}
+        '''
 
-    data = json.dumps(a, default=decimal_default)
+        cursor2 = connection.cursor()
+        d = "trace " + request.args.get('query')
+        cursor2.execute(d)
+        e = cursor2.fetchall()
 
-    a = json.loads(data)
+        xVals = []
+        yVals = []
+
+        column_descriptions = cursor.description
+        chartNames = []
+        for x in column_descriptions:
+            chartNames.append(x[0])
+
+        for x in b:
+            xVals.append(x[0]+x[1])
+        count = 2
+        for x in range((len(b[0])) - 2):
+            yInnerList = []
+            for y in b:
+                yInnerList.append(y[count])
+            count += 1
+            yVals.append(yInnerList)
+
+        timeTook = e[-1][0]
+
+        a = {"chartNames": chartNames, "yVals": yVals, "xVals": xVals, "runTime": timeTook}
+
+        data = json.dumps(a, default=decimal_default)
+
+        a = json.loads(data)
 
     return render_template("bar_graph_full.html", data=a, timeTook=a.get("runTime"))
 
